7e1d33fac8c25ab7d8c3ace35a993190113e88a5
André Maree <ammaree@users.noreply.github.com>
2025-05-07 22:17:42 +0200

    Update paho_mqtt.c

--- paho_mqtt.c
+++ paho_mqtt.c
@@ -135,7 +135,7 @@ int	xMqttWrite(Network * psNetwork, u8_t * buffer, i16_t i16Len, u32_t mSecTime)
 	netx_t * psCtx = &psNetwork->sCtx;
 	IF_EXEC(debugTRACK, psCtx->d.d = psCtx->d.w = xOptionGet(dbMQTTrw) & 2 ? 1 : 0);
 	psCtx->tOut = mSecTime;
-	return  xNetSend(psCtx, buffer, i16Len);
+	return xNetSend(psCtx, buffer, i16Len);
 }
 
 void vMqttNetworkInit(Network * psNetwork) {
